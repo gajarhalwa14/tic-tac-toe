f2f7b5ca527643806c9d2c19073f0a871ce10f0d
gajarhalwa14 <konnursunay@gmail.com>
2025-07-11 18:51:22 +0000

    Finished integrating display controller with gameboard, so now tic tac toe display is functional

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const gameBoard = (function () {
     ];
 
     let currentMarkerTurn = 'X';
+    let gameFinished = false;
+    const getGameFinished = () => gameFinished;
+    const setGameFinished = (value) => gameFinished = value; 
     const isSpotTaken = (index) => array[index] === '-' ? false : true;
     const checkRows = (markerType) => {
         for (let i = 0; i <=6; i += 3) {
@@ -61,6 +64,7 @@ const gameBoard = (function () {
         return true;
     }
     const addMarker = (markerType, index) => array[index] = markerType;
+    const getMarkerAtIndex = (index) => array[index];
     const getCurrentMarkerTurn = () => currentMarkerTurn;
     const changeCurrentMarkerTurn = () => currentMarkerTurn === 'X' ? currentMarkerTurn = 'O' : currentMarkerTurn = 'X';
     const makeMove = (index) => {
@@ -68,33 +72,74 @@ const gameBoard = (function () {
             alert("Invalid move!");
             return;
         }
+        if (getGameFinished()) {
+            return;
+        }
         addMarker(currentMarkerTurn, index);
+        displayController.displaySquare(index);
         printBoard();
         if (checkGameWon(currentMarkerTurn)) {
+            const outputText = document.querySelector(".output");
+            outputText.innerHTML = currentMarkerTurn + " won!";
             console.log(currentMarkerTurn + " won!");
+            setGameFinished(true);
             return;
         }
         changeCurrentMarkerTurn();
         if (isEverySpotTaken()) {
+            const outputText = document.querySelector(".output");
+            outputText.innerHTML = "Game ends in a draw!";
             console.log("Game ends in a draw!");
+            setGameFinished(true);
             return;
         }
     }
+    const resetBoard = () => {
+        currentMarkerTurn = 'X';
+        array = ['-', '-', '-',
+                 '-', '-', '-',
+                 '-', '-', '-'
+        ];
+        const childArr = gameDiv.children;
+        for (child of childArr) {
+            child.innerHTML = "";
+        }
+        const outputText = document.querySelector(".output");
+        outputText.innerHTML = "";
+        setGameFinished(false);
+    }
     const printBoard = () => console.log(`${array[0]} ${array[1]} ${array[2]}\n${array[3]} ${array[4]} ${array[5]}\n${array[6]} ${array[7]} ${array[8]}\n`)
-    return {isSpotTaken, checkGameDraw, checkGameWon, addMarker, makeMove, getCurrentMarkerTurn,
-        changeCurrentMarkerTurn, printBoard};
+    return {isSpotTaken, checkGameDraw, checkGameWon, addMarker, makeMove, getCurrentMarkerTurn, getMarkerAtIndex,
+        changeCurrentMarkerTurn, printBoard, resetBoard};
 })();
 
-gameBoard.printBoard();
-gameBoard.makeMove(1);
-gameBoard.makeMove(3);
-gameBoard.makeMove(4);
-gameBoard.makeMove(8);
-gameBoard.makeMove(6);
-gameBoard.makeMove(7);
-gameBoard.makeMove(5);
-gameBoard.makeMove(2);
-gameBoard.makeMove(0);
+
+
+const displayController = (function () {
+    const displaySquare = (index) => {
+        const marker = gameBoard.getMarkerAtIndex(index);
+        if ((marker === '-')) {
+            return;
+        }
+        gameDiv.children[index].innerHTML = marker;
+    }
+
+    const getIndexOfDivClicked = (target) => Number(target.id);
+    return {displaySquare, getIndexOfDivClicked}
+})();
+
+const gameDiv = document.querySelector(".game-div");
+gameDiv.addEventListener("click", (e) => {
+    gameBoard.makeMove(displayController.getIndexOfDivClicked(e.target));
+});
+
+const resetButton = document.querySelector("#reset-btn");
+resetButton.addEventListener("click", () => {
+    gameBoard.resetBoard();
+})
+
+
+
 
 
 
